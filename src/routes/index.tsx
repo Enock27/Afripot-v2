82caf5339d4bb21c6cc17295d451c213f0a17f67
Enock Mbagariye <enoxxreb@gmail.com>
2026-07-17 11:57:38 +0200

    xxxmk

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -124,21 +124,7 @@ function Index() {
                 Where tradition meets taste
               </p>
 
-              {/* Michelin flower icons */}
-              <div className="mt-4 sm:mt-5 md:mt-6 lg:mt-7 flex items-center justify-center md:justify-end gap-2 sm:gap-3 md:gap-4">
-                <svg width="24" height="24" viewBox="0 0 36 36" fill="none" aria-label="Michelin star" className="sm:w-8 sm:h-8 md:w-9 md:h-9" xmlns="http://www.w3.org/2000/svg">
-                  <circle cx="18" cy="18" r="4" fill="white"/>
-                  {[0,45,90,135,180,225,270,315].map((deg, i) => (
-                    <ellipse key={i} cx="18" cy="9" rx="3.5" ry="5.5" fill="white" transform={`rotate(${deg} 18 18)`} opacity="0.9"/>
-                  ))}
-                </svg>
-                <svg width="24" height="24" viewBox="0 0 36 36" fill="none" aria-label="Michelin star" className="sm:w-8 sm:h-8 md:w-9 md:h-9" xmlns="http://www.w3.org/2000/svg">
-                  <circle cx="18" cy="18" r="4" fill="white"/>
-                  {[0,45,90,135,180,225,270,315].map((deg, i) => (
-                    <ellipse key={i} cx="18" cy="9" rx="3.5" ry="5.5" fill="white" transform={`rotate(${deg} 18 18)`} opacity="0.9"/>
-                  ))}
-                </svg>
-              </div>
+           
             </div>
           </div>
 
